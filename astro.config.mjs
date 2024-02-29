7c6d6148a39bd533c648f39a5380e6e2b22cdc4a
DaKheera47 <shaheergoodboy@gmail.com>
2024-02-29 06:25:05 +0000

    Add redirect for /vector-ai/products to /vector-ai/products/vfit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    "/vector-ai/products": "/vector-ai/products/vfit",
+  },
   integrations: [tailwind(), icon(), react()],
   site: "https://project-mirage.github.io/vector-ai/",
   base: "/vector-ai",
