f299790d0ec948e4e50ce4dbd7199d5a9da1acaa
DaKheera47 <shaheergoodboy@gmail.com>
2024-03-05 13:42:32 +0000

    Compressing files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    "/vector-ai/products": "/vector-ai/products/vfit",
+    "/vector-ai/products": "/vector-ai/products/vfit"
   },
-  integrations: [tailwind(), icon(), react()],
+  integrations: [tailwind(), icon(), react(), compress()],
   site: "https://project-mirage.github.io/vector-ai/",
-  base: "/vector-ai",
+  base: "/vector-ai"
 });
